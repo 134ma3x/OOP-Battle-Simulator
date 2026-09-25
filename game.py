a9384c97c10effe3f4ba920379f4c0a81d73d542
134ma3x <jmdobbins0524@gmail.com>
2026-09-25 08:56:48 -0500

    battle loop and more goblins

--- game.py
+++ game.py
@@ -1,9 +1,24 @@
-from goblin import Goblin
+import goblin
 import hero
 
 
 ARENA_NAME = "The Greatest Arena to Exist"
 
+def battle(hero:hero.Hero, enemy:goblin.Goblin):
+    """battle between players/npcs"""
+    while hero.is_alive() and enemy.is_alive():
+        enemy.take_damage(hero.attack())
+        if enemy.is_alive():
+            hero.take_damage(enemy.attack())
+        print(f"{hero.name} has {hero.health} health")
+        print(f"{enemy.name} has {enemy.health} health")
+
+    if hero.is_alive():
+        print(f"{hero.name} won")
+        return
+    if enemy.is_alive():
+        print(f"{enemy.name} won")
+        return
 
 def main():
     """Open the arena and introduce its first opponent."""
@@ -14,8 +29,9 @@ def main():
     print("༼ ᓄºل͟º ༽ᓄ   ᕦ(ò_óˇ)ᕤ")
     print("The gates are opening...")
 
-    goblin = Goblin("Bribble")
-    new_goblin = Goblin("Scribble")
+    goblins = []
+    for i in range(3):
+        goblins.append(goblin.Goblin(name=i))
 
     goblin.take_damage(character.gambit())
     print(f"Is the goblin still alive? {goblin.is_alive()} with {goblin.health}hp")
@@ -23,3 +39,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+    for gobby in goblins:
+        battle(character, gobby)
